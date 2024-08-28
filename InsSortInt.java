46b7bbdfa37a9ed39f7fb5dc0e8b5735f990998b
Andrew Doyle <106994280+a-doyle2@users.noreply.github.com>
2024-08-28 19:36:15 +0000

    Submitted

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,37 @@
-public class InsSortInt {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
+public class InsSortInt {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("randInts.txt"));
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            arr1.add(scanner.nextInt());
+        }
+        scanner.close();
+        
+        Integer[] arr = new Integer[arr1.size()];
+        arr1.toArray(arr);
+
+        insertionSort(arr);
+        
+        System.out.println("Smallest: "+arr[0]+"\nLargest: "+arr[arr.length-1]);
+    }
+
+    public static int insertionSort(Integer[] arr) {
+        int steps = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int element = arr[i];
+            int current = i-1;
+            while (current >= 0 && arr[current] > element) {
+                arr[current + 1] = arr[current];
+                current--;
+                steps++;
+            }
+            arr[current+1] = element;
+        }
+        return steps;
+    }
 }
